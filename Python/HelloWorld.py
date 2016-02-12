bdfb16af3048cdcb303598827710219477799dcb
Srivastava <srivastavak11@gmail.com>
2016-02-11 21:58:03 -0800

    Third Push

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,15 +1,33 @@
 from sys import argv
 
-script, from_file, to_file = argv
+script, input_file = argv
 
-print "Copying from files %s to %s" %(from_file, to_file)
+def print_all(f):
+    print f.read()
 
-infile=open(from_file)
+def rewind(f):
+    f.seek(0)
 
-indata=infile.read()
+def print_a_line(line_count, f):
+    print line_count, f.readline(),
 
-outFile=open(to_file,'w')
-outFile.write(indata)
+current_file = open(input_file)
 
-outFile.close()
-infile.close();
+print "First let's print the whole file:\n",
+
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape.",
+
+rewind(current_file)
+
+print "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
