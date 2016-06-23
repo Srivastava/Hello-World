1aa898483e7cfbf9cd246843816d9b17a58dddc9
Srivastava <srivastavak11@gmail.com>
2016-06-22 18:38:18 -0700

    latest Push

--- SPOJ/ARITH2.cpp
+++ SPOJ/ARITH2.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <iterator>
 #include <algorithm>
+#include <string>
 
 /***************************************************************
 /********************                 *******************************************
@@ -15,6 +16,72 @@
 /********************                 *******************************************
 /***************************************************************/
 
+int add(const int& n1,const int& n2)
+{
+	return n1+n2;
+}
+
+int mul(const int& n1,const int& n2)
+{
+	return n1*n2;
+}
+int divi(const int& n1,const int& n2)
+{
+	return n1/n2;
+}
+int sub(const int& n1,const int& n2)
+{
+	return n1-n2;
+}
+
+int result(const std::string& str)
+{
+	std::string temp="";
+	int prev=0,curr=0,count=0,val=0;
+	for(int i=0;i<str.size();++i)
+	{
+		char c=str[i];
+
+
+		switch(c)
+		{
+			case '+' :
+				curr=std::stoi(temp);
+				prev=add(prev,curr);
+				++count;temp="";
+				break;
+			case '*' :
+				curr=std::stoi(temp);
+				if(count==0){prev=1;}
+				prev=mul(prev,curr);
+				++count;temp="";
+				break;
+			case '-' :
+				curr=std::stoi(temp);
+				prev=sub(prev,curr);
+				++count;temp="";
+				break;
+			case '/' :
+				curr=std::stoi(temp);
+				if(count==0){prev=1;}
+				prev=divi(prev,curr);
+				++count;temp="";
+				break;
+			case '=':
+				break;
+			default:
+				temp+=c;
+				break;
+		}
+			
+
+			c=str[i];
+			if(c=='='){break;}
+		
+
+	}
+	return prev;
+}
 int main()
 {
 	int nInputs;
@@ -30,7 +97,12 @@ int main()
 		{
 			--nInputs;
 			std::cout<<str<<std::endl;
+			str.erase(std::remove(str.begin(),str.end(),' '),str.end());
+			std::cout<<str<<std::endl;
+			std::cout<<result(str)<<std::endl;
 		}
+
+
 		// std::cin>>str;
 
 	}
